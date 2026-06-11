57cb70715d9a5a80d7d4694eebccfcb27b527008
Lauren Hunt <someoddsense@Laurens-MacBook-Pro-31.local>
2026-06-11 13:26:08 -0500

    paper testing

--- pages/4_Capstone_Paper.py
+++ pages/4_Capstone_Paper.py
@@ -4,12 +4,13 @@ import streamlit as st
 pdf_url = "https://raw.githubusercontent.com/some0ddsense/Chicago_LandUse_App/main/docs/Chicago_Land_Use_Explorer.pdf"
 
 pdf_display = f"""
-<embed
+<iframe
     src="{pdf_url}"
-    type="application/pdf"
     width="100%"
     height="900px"
-/>
+    style="border: none;">
+</iframe>
+<p><a href="{pdf_url}" target="_blank">Open PDF in new tab</a></p>
 """
 
 st.markdown(pdf_display, unsafe_allow_html=True)
