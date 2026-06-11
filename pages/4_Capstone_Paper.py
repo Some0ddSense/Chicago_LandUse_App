0611bf29fb1fadc912a55682e03ddbdc9de4d8f1
Lauren Hunt <someoddsense@Laurens-MacBook-Pro-31.local>
2026-06-11 12:53:43 -0500

    Still testing

--- pages/4_Capstone_Paper.py
+++ pages/4_Capstone_Paper.py
@@ -1,13 +1,19 @@
 import streamlit as st
 import base64
 
-pdf_path = "docs/Chicago_Land_Use_Explorer.pdf"
+pdf_path = "docs/chicago_land_use_explorer.pdf"
 
 with open(pdf_path, "rb") as f:
     base64_pdf = base64.b64encode(f.read()).decode("utf-8")
 
 pdf_display = f"""
-<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="800"></iframe>
+<embed
+    src="data:application/pdf;base64,{base64_pdf}"
+    type="application/pdf"
+    width="100%"
+    height="800"
+/>
 """
 
 st.markdown(pdf_display, unsafe_allow_html=True)
+
