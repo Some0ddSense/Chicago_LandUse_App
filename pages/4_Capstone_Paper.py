c65f0df5d8594cc82ea9b8f6f454e9baf5f03e75
Lauren Hunt <someoddsense@Laurens-MacBook-Pro-31.local>
2026-06-11 13:04:17 -0500

    testing still

--- pages/4_Capstone_Paper.py
+++ pages/4_Capstone_Paper.py
@@ -1,5 +1,21 @@
 import streamlit as st
+import base64
+from pathlib import Path
 
-st.pdf("docs/Chicago_Land_Use_Explorer.pdf")
+pdf_path = Path("docs/Chicago_Land_Use_Explorer.pdf")
+
+with open(pdf_path, "rb") as f:
+    base64_pdf = base64.b64encode(f.read()).decode("utf-8")
+
+pdf_display = f"""
+<embed
+    src="data:application/pdf;base64,{base64_pdf}"
+    type="application/pdf"
+    width="100%"
+    height="900px"
+/>
+"""
+
+st.markdown(pdf_display, unsafe_allow_html=True)
 
 
