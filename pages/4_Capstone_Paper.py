ab03b0a30100de60c6bf4d1bd0292496f4f7bb4f
Lauren Hunt <someoddsense@Laurens-MacBook-Pro-31.local>
2026-06-11 12:42:45 -0500

    Display Paper

--- pages/4_Capstone_Paper.py
+++ pages/4_Capstone_Paper.py
@@ -1,3 +1,13 @@
 import streamlit as st
+import base64
 
-st.write(""" TO BE ADDED """)
+pdf_path = "docs/Chicago Land Use Explorer.pdf"
+
+with open(pdf_path, "rb") as f:
+    base64_pdf = base64.b64encode(f.read()).decode("utf-8")
+
+pdf_display = f"""
+<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="800"></iframe>
+"""
+
+st.markdown(pdf_display, unsafe_allow_html=True)
