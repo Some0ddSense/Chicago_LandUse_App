39363f15a5a1a58ceca36666725ac6f865344630
Lauren Hunt <someoddsense@Laurens-MacBook-Pro-31.local>
2026-06-11 13:29:33 -0500

    testing

--- pages/4_Capstone_Paper.py
+++ pages/4_Capstone_Paper.py
@@ -1,18 +1,11 @@
 import streamlit as st
 
-# Use raw GitHub URL for reliable deployment
-pdf_url = "https://raw.githubusercontent.com/some0ddsense/Chicago_LandUse_App/main/docs/Chicago_Land_Use_Explorer.pdf"
+# Use Google Docs Viewer to display PDF inline
+raw_pdf_url = "https://raw.githubusercontent.com/some0ddsense/Chicago_LandUse_App/main/docs/Chicago_Land_Use_Explorer.pdf"
+viewer_url = f"https://docs.google.com/viewer?url={raw_pdf_url}&embedded=true"
 
-pdf_display = f"""
-<iframe
-    src="{pdf_url}"
-    width="100%"
-    height="900px"
-    style="border: none;">
-</iframe>
-<p><a href="{pdf_url}" target="_blank">Open PDF in new tab</a></p>
-"""
-
-st.markdown(pdf_display, unsafe_allow_html=True)
+st.markdown(f"""
+<iframe src="{viewer_url}" width="100%" height="900px"></iframe>
+""", unsafe_allow_html=True)
 
 
